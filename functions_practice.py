b4129f39beccd553d2a3cd0812c4fef40d9026dd
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-08-01 19:09:57 -0700

    completed

---
+++ functions_practice.py
@@ -0,0 +1,24 @@
+def hello():
+    print("Hello, how are you!")
+
+
+def pack(peram1,peram2,peram3):
+    return [1,2,3]
+    
+
+def eat_lunch(list):
+    if len(list) == 0:
+        print("My Lunchbox is empty")
+    else:
+        for i in range(len(list)):
+            if i == 0:
+                print(f"First I eat {list[0]}")
+            else:
+                print(f"Next I eat {list[i]}")
+
+hello()
+print(pack("1","2","3"))
+print(pack(1,2,3))
+eat_lunch(["cookie"])
+eat_lunch(["pie","sandwich","juice","beagle"])
+eat_lunch([])
